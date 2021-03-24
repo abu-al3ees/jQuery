2979844fc2267492ed27b9c95d5fa70b27c19f6d
abu-al3ees <malkhazali83@gmail.com>
2021-03-24 17:39:07 +0200

    00000

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-undef */
 /* eslint-disable eqeqeq */
 /* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
+//////////////////////
 'use strict';
 let keywords = [];
 let infoArray = [];
